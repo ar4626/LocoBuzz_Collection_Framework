388f344b47bcc4e19f6475add4f53f75223dea39
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-03 15:08:40 +0530

    [Ankit] Stack : Various operations on Stack

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Collection_Framework
     {
         static void Main(string[] args)
         {
-            List<int> arr;
+            /*List<int> arr;
             arr = new List<int>();
 
             arr.Add(1);
@@ -20,7 +21,31 @@ namespace Collection_Framework
             arr.Add(5);
 
             Console.WriteLine(arr.Count);   
-            Console.WriteLine(arr.Contains(1));
+            Console.WriteLine(arr.Contains(1));*/
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            Console.WriteLine(stack.Count());
+
+            Stack stack1 = new Stack();
+            stack1.Push(1);
+            stack1.Push(2);
+            stack1.Pop();
+            stack1.Push(4);
+            stack1.Push(5);
+            Console.WriteLine(stack1.Count);
+            Console.WriteLine(stack1.Peek());
+
+
+            //Various operations for stack are 
+            //Push()
+            //Pop()
+            //Peek
+
         }
     }
 }
