1bf7977fe98f62cb38d0e8bf88f9840e4d88942c
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-03 15:18:18 +0530

    [Ankit] Queue : Various operations on Queue

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace Collection_Framework
             Console.WriteLine(arr.Count);   
             Console.WriteLine(arr.Contains(1));*/
 
-            Stack<int> stack = new Stack<int>();
+            /*Stack<int> stack = new Stack<int>();
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
@@ -38,13 +38,31 @@ namespace Collection_Framework
             stack1.Push(4);
             stack1.Push(5);
             Console.WriteLine(stack1.Count);
-            Console.WriteLine(stack1.Peek());
+            Console.WriteLine(stack1.Peek());*/
 
 
             //Various operations for stack are 
             //Push()
             //Pop()
             //Peek
+
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Dequeue();
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            Console.WriteLine(queue.Count());
+
+            Stack queue1 = new Stack();
+            queue1.Push(1);
+            queue1.Push(2);
+            queue1.Pop();
+            queue1.Push(4);
+            queue1.Push(5);
+            Console.WriteLine(queue1.Count);
+            Console.WriteLine(queue1.Peek());
 
         }
     }
