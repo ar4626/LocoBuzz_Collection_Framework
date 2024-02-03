e7052f5744e64381438f469c0441c9995bcf1e5c
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-03 14:57:02 +0530

    [Ankit] List : Various operations on List

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace Collection_Framework
     {
         static void Main(string[] args)
         {
+            List<int> arr;
+            arr = new List<int>();
+
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+            Console.WriteLine(arr.Count);   
+            Console.WriteLine(arr.Contains(1));
         }
     }
 }
